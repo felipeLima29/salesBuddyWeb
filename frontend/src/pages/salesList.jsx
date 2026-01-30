f971cd1d8617a2ea5bb3d11ccc0d48f17d119025
Felipe <felipe.pereira9012@gmail.com>
2026-01-30 13:59:54 -0300

    feat: changes

--- frontend/src/pages/salesList.jsx
+++ frontend/src/pages/salesList.jsx
@@ -44,12 +44,12 @@ function SalesList() {
                     <thead>
                         <tr>
                             <td className="txViewHeader textCenter">ID.VENDA</td>
-                            <td className="txViewHeader">NOME</td>
-                            <td className="txViewHeader">CPF</td>
-                            <td className="txViewHeader">E-MAIL</td>
+                            <td className="txViewHeader textLeft">NOME</td>
+                            <td className="txViewHeader textLeft">CPF</td>
+                            <td className="txViewHeader textLeft">E-MAIL</td>
                             <td className="txViewHeader textCenter">QTD.ITENS</td>
-                            <td className="txViewHeader textCenter">VALOR</td>
-                            <td className="txViewHeader textCenter">TROCO</td>
+                            <td className="txViewHeader textCenter colSpacing">VALOR</td>
+                            <td className="txViewHeader textCenter colSpacing">TROCO</td>
                             <td className="txViewHeader textCenter">COMPROVANTE</td>
 
                         </tr>
@@ -58,14 +58,16 @@ function SalesList() {
                         {sales.map((sale) => (
                             <tr key={sale.idVenda}>
                                 <td className="txViewIdName textCenter">{sale.id}</td>
-                                <td className="txViewIdName">{sale.name}</td>
-                                <td className="txViewUsersSales">{formatCpf(sale.cpf)}</td>
-                                <td className="txViewUsersSales">{sale.email}</td>
-                                <td className="txViewUsersSales textCenter">{sale.qtdItems}</td>
-                                <td className="txViewUsersSales textCenter">{sale.valueSale}</td>
-                                <td className="txViewUsersSales textCenter">{sale.changeDue}</td>
+                                <td className="txViewIdName textLeft">{sale.name}</td>
+                                <td className="txViewUsersSales textLeft">{formatCpf(sale.cpf)}</td>
+                                <td className="txViewUsersSales textLeft">{sale.email}</td>
+                                <td className="txViewUsersSales textCenter">{"0" + sale.qtdItems}</td>
+                                <td className="txViewUsersSales textCenter colSpacing">{"R$ " + sale.valueSale}</td>
+                                <td className="txViewUsersSales textCenter colSpacing">{"R$" + sale.changeDue}</td>
                                 <td>
-                                    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", paddingBottom: "13px" }}>
+                                    <div style={{ display: "flex", justifyContent: "center",
+                                        alignItems: "center",
+                                        paddingBottom: "13px" }}>
                                         <img src={icReceipt} alt="Comprovante" onClick={() => { handleSelectReceipt(sale) }} />
                                     </div>
                                 </td>
